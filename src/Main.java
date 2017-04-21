c5d18072661168a758733e43482a68f9ce162af5
jinan.feng <jinan.feng@jinfeng-bi.com>
2017-04-21 15:14:03 +0800

    2017-4-21
    修改了代码

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        String str="hello world";
+        System.out.println(str);
 
     }
 }
